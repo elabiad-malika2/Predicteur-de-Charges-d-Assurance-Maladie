6b63b4f127aaccb62caf37eabc361595ce9b152f
elabiad-malika2 <elabiadmalika64@gmail.com>
2025-09-29 15:08:35 +0100

    PDCAM-8 Informations generales sur dataframe (nbr linges / nom et tupe / nbr valeur non nulle /tailles memoire)

--- src/data_processing/data_loader.py
+++ src/data_processing/data_loader.py
@@ -5,12 +5,4 @@ df=pd.read_csv("./data/raw/assurance-maladie.csv")
 # Fournit des statistiques descriptives sur les colonnes numériques
 print(df.describe())
 
-# Donne des informations générales sur le DataFrame :
-    # Nombre de lignes et de colonnes
-    # Nom et type de chaque colonne 
-    # Nombre de valeurs non nulles(voir les données manquantes).
-    # Taille mémoire utilisée.
-print(df.info())
 
-# Affiche les 5 premières lignes du DataFrame
-print("Affichage",df.head())
